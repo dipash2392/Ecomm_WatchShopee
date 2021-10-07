36440a124e2fb2fa6a11e6f9bd9fc74a5026b736
Dipa <dipashri2317kadam@gmail.com>
2021-10-08 02:47:34 +0530

    resolved warnings

--- src/Component/HomePage/__test__/Homepage.test.js
+++ src/Component/HomePage/__test__/Homepage.test.js
@@ -2,26 +2,9 @@ import { render, screen, cleanup } from "@testing-library/react";
 import { Provider } from "react-redux";
 import HomePage from "../HomePage";
 import store from "../../../Redux/store";
-import { act } from "react-dom/test-utils";
-import ReactDOM from "react-dom";
 import axios from "axios";
 import Products from "../../Services/homePageService";
-let container;
 
-// global.fetch = jest.fn(() =>
-//   Promise.resolve({
-//     json: () =>
-//       resolve({
-//         id: 1,
-//         productImg: "watch_1",
-//         productName: "JYSHVI ENTERPRISES Smart watch",
-//         price: 899,
-//         ratings: 2,
-//         highlights: "With Call Function. Touchscreen. Fitness and outdoor.",
-//         description: "Description",
-//       }),
-//   })
-// );
 
 const MockHome = () => {
   return (
